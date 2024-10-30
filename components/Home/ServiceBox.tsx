90c0bbae0679e51730b6835c82d0313dd6dfd491
Divine Ikhuoria <divuzki@gmail.com>
2024-10-30 11:35:10 +0100

    Updated

--- components/Home/ServiceBox.tsx
+++ components/Home/ServiceBox.tsx
@@ -9,7 +9,7 @@ interface ServiceBoxProps {
 
 const ServiceBox: React.FC<ServiceBoxProps> = ({ title, description, imageSrc, imageAlt }) => {
   return (
-    <article className="flex overflow-hidden flex-col self-stretch p-16 my-auto bg-stone-600 min-w-[240px] rounded-[40px] w-[600px] max-md:px-5 max-md:max-w-full">
+    <article className="flex overflow-hidden flex-col self-stretch p-16 my-auto bg-[#547C50] min-w-[240px] rounded-[40px] w-[600px] max-md:px-5 max-md:max-w-full">
       <img loading="lazy" src={imageSrc} alt={imageAlt} className="object-contain max-w-full rounded-3xl aspect-[2.18] w-[480px]" />
       <div className="flex flex-col justify-center mt-20 w-full max-md:mt-10 max-md:max-w-full">
         <h3 className="text-5xl font-bold tracking-tighter text-white max-md:max-w-full max-md:text-4xl">
